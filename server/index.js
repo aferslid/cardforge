df5b83de336662c456e33f2c6331e550ef95f933
aferslid <aferslidou@gmail.com>
2026-06-04 23:03:32 +0200

    Fix CORS preflight

--- server/index.js
+++ server/index.js
@@ -21,7 +21,12 @@ const upload = multer({
   storage: multer.memoryStorage(),
 });
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
+}));
+app.options("*", cors());
 app.use(express.json());
 
 app.post("/extract", async (req, res) => {
