b9d2094159936f1e4921982bf0b449df5c9b5572
aferslid <aferslidou@gmail.com>
2026-06-17 18:34:33 +0200

    17062026

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Analytics } from '@vercel/analytics/react'
+import Clarity from '@microsoft/clarity';
+
+Clarity.init("x8j7n3m9xb")
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
